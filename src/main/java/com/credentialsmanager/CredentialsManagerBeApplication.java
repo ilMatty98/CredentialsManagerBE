7e7c4a5b41b553163ce4d818a22d514436158be3
ilMatty98 <ilmatty98s@gmail.com>
2023-04-04 15:30:57 +0200

    First commit

---
+++ src/main/java/com/credentialsmanager/CredentialsManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.credentialsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsManagerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredentialsManagerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/credentialsmanager/CredentialsManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credentialsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsManagerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
