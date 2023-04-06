f0acc9c9b27bd89b86c805061eb6c079a13e6b94
ilMatty98 <ilmatty98s@gmail.com>
2023-04-06 10:35:57 +0200

    Fix import

--- src/main/java/com/credentialsmanager/controller/AuthenticationControllerImpl.java
+++ src/main/java/com/credentialsmanager/controller/AuthenticationControllerImpl.java
@@ -3,7 +3,6 @@ package com.credentialsmanager.controller;
 import com.credentialsmanager.dto.AuthenticationDto;
 import com.credentialsmanager.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
