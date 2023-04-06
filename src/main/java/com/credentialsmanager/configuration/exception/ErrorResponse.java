be019fba22e54c0411aff3fa91f7b60011ec44f1
ilMatty98 <ilmatty98s@gmail.com>
2023-04-06 17:48:13 +0200

    Add custom exceptions

---
+++ src/main/java/com/credentialsmanager/configuration/exception/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.credentialsmanager.configuration.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Getter
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private final Date timestamp;
+    private int code;
+    private String status;
+    private String message;
+
+    public ErrorResponse() {
+        timestamp = new Date();
+    }
+
+    public ErrorResponse(HttpStatus httpStatus, String message) {
+        this();
+        this.code = httpStatus.value();
+        this.status = httpStatus.name();
+        this.message = message;
+    }
+}
