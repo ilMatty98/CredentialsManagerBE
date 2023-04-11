8aa7d1b1881f58ab5eb8d3f71bb796db91c8c722
ilMatty98 <ilmatty98s@gmail.com>
2023-04-11 17:35:10 +0200

    CrossOrigin * for tests

--- src/main/java/com/credentialsmanager/controller/AuthenticationController.java
+++ src/main/java/com/credentialsmanager/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/authentication")
 @Tag(name = "Authentication")
+@CrossOrigin("*")
 public interface AuthenticationController {
 
     @PostMapping("/signUp")
