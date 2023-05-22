e114c1ce0ce8ed15b789c284cb89d5efdefe89d7
ilMatty98 <ilmatty98s@gmail.com>
2023-05-22 17:18:53 +0200

    Fix regex

--- src/main/java/com/credentialsmanager/dto/LogInDto.java
+++ src/main/java/com/credentialsmanager/dto/LogInDto.java
@@ -11,7 +11,7 @@ public interface LogInDto {
 
     @Data
     class Request {
-        @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+        @Email(message = "Email is not valid", regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
         @NotBlank(message = "Email cannot be blank")
         private String email;
 
